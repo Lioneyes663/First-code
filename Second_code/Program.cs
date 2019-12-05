299a39be89ed61337b1ebd5625b3a1f8ff44a68c
Lioneyes663 <levalano@gmail.com>
2019-12-05 20:23:32 +0500

    Second_code

--- Second_code/Program.cs
+++ Second_code/Program.cs
@@ -6,25 +6,32 @@ namespace Second_code
     {
         static void Main(string[] args)
         {
+            string name;
+            string age;
+            string education;
+            double height;
+            int easy;
+            int mid;
+            int x;
             Console.Write("Ваше имя: ");
-            string Name = Console.ReadLine();
+            name = Console.ReadLine();
             Console.Write("\nВаш возраст: ");
-            string Age = Console.ReadLine();
+            age = Console.ReadLine();
             Console.Write("\nВаше образование:");
-            string Education = Console.ReadLine();
+            education = Console.ReadLine();
             Console.Write("\nВаш рост: ");
-            double height = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"\n{Name} \nВозраст: {Age} \nРост: {height} \nОбразование:{Education}");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"\n{name} \nВозраст: {age} \nРост: {height} \nОбразование:{education}");
             Console.ReadKey();
             Console.Write("Решите простое уровнение: 5+5= ");
-            int easy = Convert.ToInt32(Console.ReadLine());
+            easy = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Правильный ответ 10, а ваш ответ {easy}");
             Console.Write("Теперь что нибудь посложнее например: 5*5= ");
-            int mid = Convert.ToInt32(Console.ReadLine());
+            mid = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Правильный ответ 25, а ваш ответ {mid}");
             Console.WriteLine("А что будет если вычесть из второго ответа первый?");
             Console.ReadKey();
-            int x = mid - easy;
+            x = mid - easy;
             Console.WriteLine($"Если высветится 15 то вы молодец \n{x}");
             Console.ReadKey();
         }
