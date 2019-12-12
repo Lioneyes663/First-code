37b5eeb89d2e08eb00d2cffd8b4f858ada7d39ed
Lioneyes663 <levalano@gmail.com>
2019-12-12 20:50:50 +0500

    added_new_codes

--- Fifth_code/Program.cs
+++ Fifth_code/Program.cs
@@ -4,38 +4,21 @@ namespace Fifth_code
 {
     class Program
     {
+        static bool IsPolindrome(string phrase)
+        {
+            int n = phrase.Length-1;
+            string temp = "";
+            for (; n >= 0; n--)
+            {
+                temp = temp + phrase[n];
+            }
+            return phrase == temp;
+        }
         static void Main(string[] args)
         {
-            string a;
-            string b;
-            string c;
-            string d;
-            string[] phrase = {"a", "b", "c", "d" };
-            Console.Write("\nВведите палиндром из 4 слов, по словам: ");
+            String a;
             a = Console.ReadLine();
-            Console.Write("\nВведите второе слово: ");
-            b = Console.ReadLine();
-            Console.Write("\nВведите третье слово: ");
-            c = Console.ReadLine();
-            Console.Write("\nВведите четвертое слово: ");
-            d = Console.ReadLine();
-
-            int n = phrase.Length;
-            int k = n / 2;
-            int temp;
-            for (int i = 0; i < k; i++) ;
-            {
-                temp = phrase[i];
-                phrase[i] = phrase[n - i - 1];
-                phrase[n - i - 1] = temp;
-            }
-            foreach (string i in phrase)
-            {
-                Console.WriteLine("\nЕсли вы видите фразу перевёрнутую то вы ввели палиндром");
-                Console.WriteLine($"{d} {c} {b} {a} \t");
-            }
-
-
+            Console.WriteLine(IsPolindrome(a));
         }
     }
 }
---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int i;
+            bool p = true;
+            Console.WriteLine("\tВведите длину массива");
+            int k = Convert.ToInt32 (Console.ReadLine());
+            int[] aMasInt = new int[k];
+            i = 0;
+            Console.WriteLine("\tВведите элементы массива");
+            do
+            {
+                aMasInt [i] = Convert.ToInt32 (Console.ReadLine());
+                i = i + 1;
+            }
+            while (i < aMasInt.Length);
+            int temp;
+            int temp2;
+            int o = aMasInt.Length / 2;
+            for (i = 0; i <= o ; i++)
+            {
+                temp = aMasInt[i];
+                temp2 = aMasInt[aMasInt.Length - 1 - i];
+                if (temp != temp2)
+                {
+                    p = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"\t{p}");
+        }
+    }
+}
